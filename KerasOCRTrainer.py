b005f1af23d6e8f04d1a82ef8aacb4357b43a65a
Eduard <kieser@healthqtech.com>
2019-06-04 15:28:41 +0200

    clean slate

---
+++ KerasOCRTrainer.py
@@ -0,0 +1,91 @@
+import numpy as np
+import tensorflow as tf
+
+num_classes = 201
+input_sizes = [28, 32, 48]
+kernel_sizes = [3, 5, 7]
+dropout_rates = [0.05, 0.1, 0.2, 0.4]
+n_features  = [16,32,64]
+optimizers = [tf.keras.optimizers.SGD(lr=0.01), tf.keras.optimizers.RMSprop(lr=0.0001)]
+n_conv_layers = [1,2,3]
+
+def assemble_model(
+    input_size = 28, 
+    kernel_size=5, 
+    n_conv_layers = 2, 
+    dropout_rate = 0.1, 
+    optimizer = tf.keras.optimizers.RMSprop(lr=0.0001))
+
+    imput_shape = (imput_size, input_size, 1)
+    kernel_shape = (kernel_size, kernel_size)
+
+    model = tf.keras.models.Sequential()
+
+    for i in range(n_conv_layers):
+        model.add(tf.keras.layers.Conv2D(
+            n_features, 
+            kernel_size=kernel_shape, 
+            strides=(1, 1),
+            activation='relu',
+            input_shape=input_shape))
+        model.add(tf.keras.layers.Dropout(rate=dropout_rate))    
+
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(1000, activation='relu'))
+    model.add(tf.keras.layers.Dense(num_classes, activation='softmax'))
+
+    model.compile(
+        loss=tf.keras.losses.categorical_crossentropy,
+        optimizer=optimizer,
+        metrics=[tf.keras.metrics.categorical_accuracy],)
+
+    train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(
+            # rescale=1./255,
+            shear_range=0.2,
+            # zoom_range=0.2,
+            horizontal_flip=False)
+
+    train_generator = train_datagen.flow_from_directory(
+        directory=r"./tf_files/numbers_train/",
+        target_size=(input_size, input_size),
+        color_mode="grayscale",
+        batch_size=32,
+        class_mode="categorical",
+        shuffle=True,
+        seed=42
+    )
+
+    valid_datagen = tf.keras.preprocessing.image.ImageDataGenerator(
+            # rescale=1./255,
+            shear_range=0.2,
+            # zoom_range=0.2,
+            horizontal_flip=False)
+
+    valid_generator = valid_datagen.flow_from_directory(
+        directory=r"./tf_files/numbers_test/",
+        target_size=(input_size, input_size),
+        color_mode="grayscale",
+        batch_size=32,
+        class_mode="categorical",
+        shuffle=True,
+        seed=42
+    )
+
+STEP_SIZE_TRAIN=train_generator.n//train_generator.batch_size
+STEP_SIZE_VALID=valid_generator.n//valid_generator.batch_size
+model.fit_generator(
+    generator=train_generator,
+    steps_per_epoch=STEP_SIZE_TRAIN,
+    validation_data=valid_generator,
+    validation_steps=STEP_SIZE_VALID,
+    epochs=10
+)
+
+# Save tf.keras model in HDF5 format.
+keras_file = "keras_model.h5"
+tf.keras.models.save_model(model, keras_file)
+
+# Convert to TensorFlow Lite model.
+converter = tf.lite.TFLiteConverter.from_keras_model_file(keras_file)
+tflite_model = converter.convert()
+open("converted_model.tflite", "wb").write(tflite_model)
---
+++ images_generator.py
@@ -0,0 +1,160 @@
+
+
+from PIL import Image, ImageDraw, ImageFont, ImageFilter, ImageOps
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+from glob import glob
+import os
+import shutil
+
+
+
+def create_noise_mask(width,height):
+    mask = np.random.uniform(-10,10,(height,width,3)).astype(np.uint8)
+    return mask
+
+
+def get_gaussian_mask(width,height):
+
+    center = (random.randint(-10,width+10),random.randint(-10,width+10))
+
+    dia = random.randint(1,width)
+
+    image = Image.new('RGB', (width, height), (0,0,0) )
+    draw = ImageDraw.Draw(image)
+    draw.ellipse((center[0]-dia, center[1]-dia, center[0]+180, center[1]+180),\
+                 fill=(5,5,5), outline='white')
+
+    image = image.filter(ImageFilter.GaussianBlur(radius=random.randint(15,40)))
+
+    image = (np.array(image)).astype(np.uint8)
+
+    # plt.imshow(Image.fromarray(image))
+
+    return image
+
+def generate_image(font, text, rotation, shape,text_color,back_color,text_offset, final_shape=(28,28)):
+
+    im_width = shape[0]
+    im_height = shape[1]
+
+    txt_img = Image.new('L', (im_width, im_height))
+    bak_img = Image.new('RGB', (im_width, im_height), color=back_color)
+
+    d = ImageDraw.Draw(txt_img)
+    text_width, text_height = d.textsize(text,font)
+    corner_coords = (int(im_width/2-text_width/2), int(im_height/2-text_height/2)-10)
+
+    d.text(corner_coords, text, fill=(255),font=font)
+
+    txt_img = np.array(txt_img)
+
+    border_width = np.clip(np.random.randint(-30,20,4), 0, 30)
+    txt_img[:,0:border_width[0]] = 255
+    txt_img[:,-(border_width[1]+1):] = 255
+    txt_img[0:border_width[0],:] = 255
+    txt_img[-(border_width[1]+1):,:] = 255
+
+
+    txt_img = Image.fromarray(txt_img)
+
+    txt_img = txt_img.rotate(rotation)
+
+    bak_img.paste(ImageOps.colorize(txt_img, text_color, text_color), text_offset,  txt_img)
+
+    img = bak_img
+
+    img = img.filter(ImageFilter.BLUR)
+
+    img = img.resize((im_width, im_height))
+
+    # noise_mask = create_noise_mask(im_width, im_height)
+
+    gaussian_mask = get_gaussian_mask(im_width, im_height)
+
+    np_img = np.array(img)
+
+    # np_img = np_img+noise_mask
+
+    np_img = np_img+gaussian_mask
+
+    np_img = np.clip(np_img,0,255)
+
+    im_mean = np_img.mean()
+    im_std = np_img.std()
+
+    img = Image.fromarray(np_img,'RGB')
+
+    img = img.resize(final_shape)
+
+    return img
+
+def get_dark_color():
+    return (random.randint(0,117),
+            random.randint(0,117),
+            random.randint(0,117))
+
+def get_light_color():
+    return (random.randint(117,255),
+            random.randint(117,255),
+            random.randint(117,255))
+
+
+def create_image_batch(i_start,i_stop,folder_name, final_size=28):
+
+    final_shape = (final_size,final_size)
+    
+    for i in range(i_start,i_stop):
+
+        rotation = random.randint(-20,20)
+        text_offset = (random.randint(-30,30),random.randint(-30,30))
+        number = -1
+        while number<0:
+            number = int(np.random.triangular(-20,150,200))
+        font_size = random.randint(80,110)
+        font_ix = random.randint(0,len(font_paths)-1)
+        font = ImageFont.truetype(font_paths[font_ix],font_size)
+
+        if i%2==0:
+            text_color = get_dark_color()
+            back_color = get_light_color()
+        else:
+            text_color = get_light_color()
+            back_color = get_dark_color()
+
+        original_shape = (random.randint(200,300),random.randint(150,200))
+
+
+        img = generate_image(font=font,
+                             text=str(number),
+                             rotation=rotation,
+                             shape=original_shape,
+                             back_color=back_color,
+                             text_color=text_color,
+                             text_offset=text_offset,
+                             final_shape=final_shape
+                             )
+
+        folder_path = os.path.join(
+            folder_name,
+            f'numbers_{final_size}',
+            str(number))
+
+        if not(os.path.isdir(folder_path)):
+            os.makedirs(folder_path)
+
+        img.save(os.path.join(folder_path,f"{number}-{i}.jpg"))
+
+        if i%100==0:
+            print(i)
+
+
+if __name__=='__main__':
+
+    font_paths =glob('./vitmoocr/fonts/*.ttf')
+    final_size = 96
+    folder_name = 'training_data'
+    create_image_batch(0,10000, folder_name=folder_name, final_size=final_size)
+    # folder_name = 'testing_data'
+    # create_image_batch(0,100, folder_name=folder_name, final_size=final_size)
